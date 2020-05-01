2b169bb1a5061d2477ae47516bba49e8a0c3b363
kirill5k <immotional@aol.com>
2020-05-01 11:00:34 +0100

    order repo unit tests

--- src/test/scala/io/kirill/shoppingcart/shop/order/OrderRepositorySpec.scala
+++ src/test/scala/io/kirill/shoppingcart/shop/order/OrderRepositorySpec.scala
@@ -5,16 +5,34 @@ import java.util.UUID
 import cats.effect.IO
 import io.kirill.shoppingcart.PostgresRepositorySpec
 import io.kirill.shoppingcart.auth.user.{EncryptedPassword, UserId, UserRepository, Username}
-import io.kirill.shoppingcart.common.errors.ForeignKeyViolation
-import io.kirill.shoppingcart.shop.brand.{BrandId, BrandName, BrandRepository}
-import io.kirill.shoppingcart.shop.category.{CategoryId, CategoryName, CategoryRepository}
+import io.kirill.shoppingcart.shop.cart.Quantity
+import io.kirill.shoppingcart.shop.item.ItemId
+import io.kirill.shoppingcart.shop.payment.PaymentId
 import squants.market.GBP
 
 class OrderRepositorySpec extends PostgresRepositorySpec {
 
-  "An OrderRepository" - {
+  val orderItems: Seq[OrderItem] = List(
+    OrderItem(ItemId(UUID.randomUUID()), GBP(BigDecimal(10)), Quantity(1)),
+    OrderItem(ItemId(UUID.randomUUID()), GBP(BigDecimal(5.55)), Quantity(4)),
+    OrderItem(ItemId(UUID.randomUUID()), GBP(BigDecimal(9.99)), Quantity(2))
+  )
 
+  "An OrderRepository" - {
     "find" - {
+      "return order by id" in {
+        val result = for {
+          r <- OrderRepository.make(session)
+          oid <- insertTestOrder(r)
+          o <- r.find(oid)
+        } yield o.get
+
+        result.asserting { order =>
+          order.items must be (orderItems)
+          order.totalPrice must be (GBP(BigDecimal(25.54)))
+        }
+      }
+
       "return empty option if order does not exist" in {
         val orderRepository = OrderRepository.make(session)
 
@@ -25,6 +43,20 @@ class OrderRepositorySpec extends PostgresRepositorySpec {
     }
 
     "findBy" - {
+      "return orders placed by user" in {
+        val result = for {
+          uid <- insertTestUser
+          r <- OrderRepository.make(session)
+          _ <- insertTestOrder(r)
+          orders <- r.findBy(uid)
+        } yield (orders, uid)
+
+        result.asserting { case (orders, uid) =>
+          orders must not be empty
+          orders.map(_.userId) must contain only uid
+        }
+      }
+
       "return empty list if no matches" in {
         val orderRepository = OrderRepository.make(session)
 
@@ -39,4 +71,10 @@ class OrderRepositorySpec extends PostgresRepositorySpec {
       u <- r.findByName(Username("test-user"))
       uid <- u.fold(r.create(Username("test-user"), EncryptedPassword("password")))(x => IO.pure(x.id))
     } yield uid
+
+  def insertTestOrder(repository: OrderRepository[IO]): IO[OrderId] =
+    for {
+      uid <- insertTestUser
+      oid <- repository.create(CreateOrder(uid, PaymentId(UUID.randomUUID()), orderItems, GBP(BigDecimal(25.54))))
+    } yield oid
 }
