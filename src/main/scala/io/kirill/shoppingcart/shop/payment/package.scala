373bd167029491e1c81022ae9fd641745262f4c2
kirill5k <immotional@aol.com>
2020-05-09 11:19:51 +0100

    updated card adt

--- src/main/scala/io/kirill/shoppingcart/shop/payment/package.scala
+++ src/main/scala/io/kirill/shoppingcart/shop/payment/package.scala
@@ -5,17 +5,19 @@ import java.util.UUID
 import eu.timepit.refined._
 import eu.timepit.refined.api._
 import eu.timepit.refined.boolean.And
-import eu.timepit.refined.collection.Size
-import eu.timepit.refined.string.{MatchesRegex, ValidInt}
+import eu.timepit.refined.numeric.{LessEqual, Positive}
+import eu.timepit.refined.string.{MatchesRegex}
 import io.kirill.shoppingcart.shop.order.Order
 
 package object payment {
-  type Rgx = W.`"^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$"`.T
+  type NameRegex = W.`"^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$"`.T
+  type ExpirationRegex = W.`"^[0-9]{4}$"`.T
 
-  type Name       = String Refined MatchesRegex[Rgx]
-  type Number     = Long Refined Size[16]
-  type Expiration = String Refined (Size[4] And ValidInt)
-  type CVV        = Int Refined Size[3]
+
+  type Name       = String Refined MatchesRegex[NameRegex]
+  type Number     = Long Refined (Positive And LessEqual[9999999999999999L])
+  type Expiration = String Refined MatchesRegex[ExpirationRegex]
+  type CVV        = Int Refined (Positive And LessEqual[999])
 
   final case class Card(
       name: Name,
