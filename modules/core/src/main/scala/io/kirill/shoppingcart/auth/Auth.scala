2a4bb62a8b956dd59fabf8328d177c725805aeb7
kirill5k <immotional@aol.com>
2020-05-15 20:01:55 +0100

    cleanup

--- modules/core/src/main/scala/io/kirill/shoppingcart/auth/Auth.scala
+++ modules/core/src/main/scala/io/kirill/shoppingcart/auth/Auth.scala
@@ -1,13 +1,10 @@
 package io.kirill.shoppingcart.auth
 
-import java.util.UUID
-
 import cats.effect.{Resource, Sync}
 import cats.implicits._
 import dev.profunktor.auth.jwt._
 import dev.profunktor.redis4cats.algebra.RedisCommands
-import io.circe.parser.{decode => jsonDecode}
-import io.kirill.shoppingcart.auth.user.{User, UserCacheStore, UserId, UserRepository, Username}
+import io.kirill.shoppingcart.auth.user.{UserCacheStore, UserRepository}
 import io.kirill.shoppingcart.config.AppConfig
 import pdi.jwt._
 import skunk.Session
@@ -41,12 +38,6 @@ object Auth {
       authService       <- AuthService.make(repo, caheStore, tokenGen, passwordEncryptor)
       userAuth          <- Authenticator.commonUserAuthenticator(caheStore)
       adminAuth         <- Authenticator.adminUserAuthenticator(adminJwtAuth)
-    } yield new Auth[F](
-      authService,
-      adminAuth,
-      userAuth,
-      adminJwtAuth,
-      userJwtAuth
-    )
+    } yield new Auth[F](authService, adminAuth, userAuth, adminJwtAuth, userJwtAuth)
   }
 }
